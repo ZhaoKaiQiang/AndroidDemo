af5f0ac879dffe4db8ba43344218e57d3d9b9e04
zhaokaiqiang <zhaokai_qiang@126.com>
2015-09-29 17:55:15 +0800

    init code

--- app/src/main/java/com/socks/androiddemo/ui/FloatingActionButtonActivity.java
+++ app/src/main/java/com/socks/androiddemo/ui/FloatingActionButtonActivity.java
@@ -1,6 +1,7 @@
 package com.socks.androiddemo.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 
 import com.socks.androiddemo.R;
 import com.socks.androiddemo.base.BaseActivity;
@@ -8,11 +9,13 @@ import com.socks.androiddemo.base.BaseActivity;
 
 public class FloatingActionButtonActivity extends BaseActivity {
 
+	private FloatingActionButton button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_floating_button);
-
+		button = (FloatingActionButton) findViewById(R.id.btn);
 	}
 
 }
